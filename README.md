1f9871dcce372856214bef7a7fe1ba3bd199f71e
Lucas Paskua <80064601+paskua4@users.noreply.github.com>
2023-09-22 17:50:37 -0300

    Project commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# Teste Suporte Técnico
+
+## Descrição
+
+Cenário:
+- Uma empresa solicitou um sistema simples de compra. Nesse sistema o cliente pode fazer compras em algumas empresas de acordo com os produtos que as mesmas têm disponível em estoque, além disso ele também pode ver as suas compras. Por sua vez, a empresa pode ver as suas vendas e os seus produtos.
+
+As regras de negócios são:
+
+- Cada empresa tem sua taxa (comissão do sistema) para as transações
+- Além do administrador e a própria empresa, nenhum outro usuário poderá ver
+- informações da empresa (além do nome)
+- Ao finalizar uma compra o cliente deve ver um resumo da mesma
+- O saldo da empresa deve ser alterado já refletindo as taxas
+- A empresa deve vender apenas produtos que ela esteja relacionada
+- A empresa poderá ver a taxa de comissão de sistema em cada venda (ao listar
+suas vendas)
+
+Teste
+
+- Os erros podem ser desde código, estrutura de dados, boas práticas, experiência do usuário e regras de negócio.
+- Descreva como se estivesse repassando os ajustes para um programador.
+- Em caso de erros na regra de negócio, faça um relato para a empresa que solicitou o sistema, neste relato deve ser informado o erro e porquê acontece o erro.
+- Faça o máximo de ajustes no código, de forma que as falhas sejam corrigidas. Siga a seguinte ordem para ajuste: Regra de negócio, código, boas práticas, estrutura de dados e experiência do usuário.
+
+## Descrição dos erros
+
+Falta de Restrição de Acesso as informações das Empresas pelo admin:
+- O código não implementava ações para o admin visualizar dados das empresas e não havia restrição de acesso.
+
+Alteração do Saldo da Empresa:
+- O código não atualizava o saldo da empresa corretamente, assim não refletia as taxas de comissão do sistema. Pois após criar a venda, não era atualizado o saldo da empresa subtraindo a comissão do sistema do total da venda.
+
+
+Validação de Produtos da Empresa:
+- O código não impedia a compra de produtos de outras empresas a partir da empresa selecionada, pois não havia um filtro para relacionar cada produto com a espresa em questão. Antes de adicionar um produto ao carrinho, foi necessário verificar se o produto à ser comprado era realmente um produto da empresa selecionada.
+
+- Código não possuia break no switch case (Main.java), com isso poderia ocorrer erros como: executar outros cases, sair do menu a cada iteração.
+
+- Código Deslogava sempre que executava alguma ação, como finalizar compra. Foi necessário adiciona loop While(true) para cada menu, assim o programa não é encerrado.
+
+- Menu de admin igual ao do cliente. Foi necessário implementar menu para o admin.
+
+- Refatoração dos métodos. Separação de funções que estavam dentro do switch case em métodos, para melhorar visualização, manutenção e legibilidadee do código.
+
+- Ajustes em alguns métodos da classe Usário, para manter padrão no código (isAdmin, isEmpresa, isCliente).
---
+++ src/Main.java
@@ -0,0 +1,312 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
+import java.util.stream.Collectors;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // SIMULANDO BANCO DE DADOS
+
+		List<Produto> carrinho = new ArrayList<Produto>();
+		List<Venda> vendas = new ArrayList<Venda>();
+
+		Empresa empresa = new Empresa(2, "SafeWay Padaria", "30021423000159", 0.15, 0.0);
+		Empresa empresa2 = new Empresa(1, "Level Varejo", "53239160000154", 0.05, 0.0);
+		Empresa empresa3 = new Empresa(3, "SafeWay Restaurante", "41361511000116", 0.20, 0.0);
+
+		Produto produto = new Produto(1, "Pão Frances", 5, 3.50, empresa);
+		Produto produto2 = new Produto(2, "Coturno", 10, 400.0, empresa2);
+		Produto produto3 = new Produto(3, "Jaqueta Jeans", 15, 150.0, empresa2);
+		Produto produto4 = new Produto(4, "Calça Sarja", 15, 150.0, empresa2);
+		Produto produto5 = new Produto(5, "Prato feito - Frango", 10, 25.0, empresa3);
+		Produto produto6 = new Produto(6, "Prato feito - Carne", 10, 25.0, empresa3);
+		Produto produto7 = new Produto(7, "Suco Natural", 30, 10.0, empresa3);
+		Produto produto8 = new Produto(8, "Sonho", 5, 8.50, empresa);
+		Produto produto9 = new Produto(9, "Croissant", 7, 6.50, empresa);
+		Produto produto10 = new Produto(10, "Ché Gelado", 4, 5.50, empresa);
+
+		Cliente cliente = new Cliente("07221134049", "Allan da Silva", "cliente", 20);
+		Cliente cliente2 = new Cliente("72840700050", "Samuel da Silva", "cliente2", 24);
+
+		Usuario usuario1 = new Usuario("admin", "1234", null, null);
+		Usuario usuario2 = new Usuario("empresa", "1234", null, empresa);
+		Usuario usuario3 = new Usuario("cliente", "1234", cliente, null);
+		Usuario usuario4 = new Usuario("cliente2", "1234", cliente2, null);
+		Usuario usuario5 = new Usuario("empresa2", "1234", null, empresa2);
+		Usuario usuario6 = new Usuario("empresa3", "1234", null, empresa3);
+
+		List<Usuario> usuarios = Arrays.asList(usuario1, usuario2, usuario3, usuario4, usuario5, usuario6);
+		List<Cliente> clientes = Arrays.asList(cliente, cliente2);
+		List<Empresa> empresas = Arrays.asList(empresa, empresa2, empresa3);
+		List<Produto> produtos = Arrays.asList(produto, produto2, produto3, produto4, produto5, produto6, produto7,
+				produto8, produto9, produto10);
+		executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
+	}  
+
+    public static void executar(List<Usuario> usuarios, List<Cliente> clientes, List<Empresa> empresas,
+            List<Produto> produtos, List<Produto> carrinho, List<Venda> vendas) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Entre com seu usuário e senha:");
+        System.out.print("Usuário: ");
+        String username = sc.next();
+        System.out.print("Senha: ");
+        String senha = sc.next();
+
+        Usuario usuarioLogado = userAuthentication(usuarios, username, senha);
+
+        if (usuarioLogado != null) {
+            System.out.println("Escolha uma opção para iniciar");
+            if (usuarioLogado.isAdmin()) {                
+                while(true){
+					adminMenu();
+					int escolha = sc.nextInt();
+					switch (escolha) {						
+						case 1:
+							showCompanies(empresas);
+							break;
+						case 0:
+							executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
+						case 9:
+							System.exit(0);
+						default:
+							System.out.println("Opção inválida.");
+							break;
+					}
+				}
+            } else if (usuarioLogado.isEmpresa()) {
+                // Menu para empresa
+                while(true){
+					companyMenu();
+					int escolha = sc.nextInt();
+					switch (escolha) {
+						case 1:
+							showCompanySales(usuarioLogado, vendas);
+							break;
+						case 2:
+							showCompanyProducts(usuarioLogado, produtos);
+							break;
+						case 0:
+							executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
+						case 9:
+							System.exit(0);							
+						default:
+							System.out.println("Opção inválida.");
+							break;
+					}
+				}
+            } else if (usuarioLogado.isCliente()) {
+                // Menu para cliente
+				while(true){
+					clientMenu();
+					int escolha = sc.nextInt();
+					switch (escolha) {
+						case 1:
+							makePurchase(usuarioLogado, empresas, produtos, carrinho, vendas, clientes);
+							break;
+						case 2:
+							showClientPurchase(usuarioLogado, vendas);
+							break;
+						case 0:
+							executar(usuarios, clientes, empresas, produtos, carrinho, vendas);
+						case 9:
+							System.exit(0);
+						default:
+							System.out.println("Opção inválida.");
+							break;
+					}
+				}
+            }
+        } else {
+            System.out.println("Usuário não encontrado ou senha incorreta.");
+        }
+    }
+
+    // Métodos para operações
+
+    private static Usuario userAuthentication(List<Usuario> usuarios, String username, String senha) {
+        List<Usuario> usuariosSearch = usuarios.stream().filter(x -> x.getUsername().equals(username))
+				.collect(Collectors.toList());
+		if (usuariosSearch.size() > 0) {
+			Usuario usuarioLogado = usuariosSearch.get(0);
+			if ((usuarioLogado.getSenha().equals(senha))){
+                return usuarioLogado;
+            }
+        }
+        return null;            
+    }   
+
+    private static void adminMenu() {        
+        System.out.println("1 - Listar Usuários");
+        System.out.println("2 - Listar Empresas");
+        System.out.println("0 - Deslogar");
+		System.out.println("9 - Sair");
+    }
+
+    private static void companyMenu() {        
+        System.out.println("1 - Listar Vendas");
+        System.out.println("2 - Ver Produtos");
+        System.out.println("0 - Deslogar");
+		System.out.println("9 - Sair");
+    }
+
+    private static void clientMenu() {
+        System.out.println("1 - Realizar Compras");
+	    System.out.println("2 - Ver Compras");
+	    System.out.println("0 - Deslogar");
+		System.out.println("9 - Sair");
+    }
+
+    private static void showCompanySales(Usuario empresa, List<Venda> vendas) {
+        System.out.println();
+		System.out.println("************************************************************");
+		System.out.println("VENDAS EFETUADAS");
+		final double[] taxes = { 0.00 };
+		vendas.stream().forEach(venda -> {
+			if (venda.getEmpresa().getId().equals(empresa.getEmpresa().getId())) {
+				System.out.println("************************************************************");
+				System.out.println("Venda de código: " + venda.getCódigo() + " no CPF "
+						+ venda.getCliente().getCpf() + ": ");
+				venda.getItens().stream().forEach(x -> {
+					System.out.println(x.getId() + " - " + x.getNome() + "    R$" + x.getPreco());
+				});
+				System.out.println("Total Venda: R$" + venda.getValor());
+				System.out.println("Total Taxa a ser paga: R$" + venda.getComissaoSistema());
+				System.out.println("Total Líquido  para empresa: R$"
+						+ (venda.getValor() - venda.getComissaoSistema()));
+				System.out.println("************************************************************");
+				taxes[0] += venda.getComissaoSistema();
+			}
+		});
+		System.out.println("Saldo Empresa: " + (empresa.getEmpresa().getSaldo() - taxes[0]));
+		System.out.println("************************************************************");					
+    }
+
+    private static void showCompanyProducts(Usuario empresa, List<Produto> produtos) {
+        System.out.println();
+		System.out.println("************************************************************");
+		System.out.println("MEUS PRODUTOS");
+		produtos.stream().forEach(produto -> {
+			if (produto.getEmpresa().getId().equals(empresa.getEmpresa().getId())) {
+				System.out.println("************************************************************");
+				System.out.println("Código: " + produto.getId());
+				System.out.println("Produto: " + produto.getNome());
+				System.out.println("Quantidade em estoque: " + produto.getQuantidade());
+				System.out.println("Valor: R$" + produto.getPreco());								
+				System.out.println("************************************************************");
+			}
+		});
+		System.out.println("Saldo Empresa: " + empresa.getEmpresa().getSaldo());
+		System.out.println("************************************************************");
+    }
+
+    private static void makePurchase(Usuario cliente, List<Empresa> empresas, List<Produto> produtos,
+            List<Produto> carrinho, List<Venda> vendas, List<Cliente> clientes) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Para realizar uma compra, escolha a empresa onde deseja comprar: ");
+		empresas.stream().forEach(x -> {
+			System.out.println(x.getId() + " - " + x.getNome());
+		});
+		Integer escolhaEmpresa = sc.nextInt();
+		Integer escolhaProduto = -1;
+		do {
+			System.out.println("Escolha os seus produtos: ");
+			produtos.stream().forEach(x -> {
+				if (x.getEmpresa().getId().equals(escolhaEmpresa)) {
+					System.out.println(x.getId() + " - " + x.getNome());
+				}
+			});
+			System.out.println("0 - Finalizar compra");
+			escolhaProduto = sc.nextInt();
+			for (Produto produtoSearch : produtos) {
+				if (produtoSearch.getId().equals(escolhaProduto) && escolhaEmpresa == produtoSearch.getEmpresa().getId())
+					carrinho.add(produtoSearch);
+			}
+		} while (escolhaProduto != 0);
+		System.out.println("************************************************************");
+		System.out.println("Resumo da compra: ");
+		carrinho.stream().forEach(x -> {
+			if (x.getEmpresa().getId().equals(escolhaEmpresa)) {
+				System.out.println(x.getId() + " - " + x.getNome() + "    R$" + x.getPreco());
+			}
+		});
+		Empresa empresaEscolhida = empresas.stream().filter(x -> x.getId().equals(escolhaEmpresa))
+				.collect(Collectors.toList()).get(0);
+		Cliente clienteLogado = clientes.stream()
+				.filter(x -> x.getUsername().equals(cliente.getUsername()))
+				.collect(Collectors.toList()).get(0);
+		Venda venda = criarVenda(carrinho, empresaEscolhida, clienteLogado, vendas);
+		System.out.println("Total: R$" + venda.getValor());
+		System.out.println("************************************************************");
+		carrinho.clear();
+    }
+
+    private static void showClientPurchase(Usuario cliente, List<Venda> vendas) {
+        System.out.println();
+		System.out.println("************************************************************");
+		System.out.println("COMPRAS EFETUADAS");
+		vendas.stream().forEach(venda -> {
+			if (venda.getCliente().getUsername().equals(cliente.getUsername())) {
+				System.out.println("************************************************************");
+				System.out.println("Compra de código: " + venda.getCódigo() + " na empresa "
+						+ venda.getEmpresa().getNome() + ": ");
+				venda.getItens().stream().forEach(x -> {
+					System.out.println(x.getId() + " - " + x.getNome() + "    R$" + x.getPreco());
+				});
+				System.out.println("Total: R$" + venda.getValor());
+				System.out.println("************************************************************");
+			}
+		});		
+    }
+
+    public static Venda criarVenda(List<Produto> carrinho, Empresa empresa, Cliente cliente, List<Venda> vendas) {
+		Double total = carrinho.stream().mapToDouble(Produto::getPreco).sum();
+		Double comissaoSistema = total * empresa.getTaxa();
+		int idVenda = vendas.isEmpty() ? 1 : vendas.get(vendas.size() - 1).getCódigo() + 1;
+		Venda venda = new Venda(idVenda, carrinho.stream().toList(), total, comissaoSistema, empresa, cliente);
+		empresa.setSaldo(empresa.getSaldo() + total);
+		vendas.add(venda);
+		return venda;
+	}
+
+	private static void showCompanies(List<Empresa> empresas) {
+		Scanner sc = new Scanner(System.in);
+
+		System.out.println("Escolha o ID da empresa para ver os detalhes: ");
+		empresas.stream().forEach(x -> {
+			System.out.println(x.getId() + " - " + x.getNome());
+		});
+		System.out.println("0 - Deslogar");
+
+		Integer escolhaEmpresa = sc.nextInt();
+
+		if (escolhaEmpresa == 0) {
+        	return;
+    	}
+
+		Empresa empresaSelecionada = null;
+		for (Empresa empresa : empresas) {
+			if (empresa.getId() == escolhaEmpresa) {
+				empresaSelecionada = empresa;
+				break;
+			}
+		}
+
+		if (empresaSelecionada != null) {			
+			System.out.println("Detalhes da Empresa:");
+			System.out.println("ID: " + empresaSelecionada.getId());
+			System.out.println("Nome: " + empresaSelecionada.getNome());
+			System.out.println("CNPJ: " + empresaSelecionada.getCnpj());
+			System.out.println("Taxa: " + empresaSelecionada.getTaxa());			
+		} else {
+			System.out.println("Empresa não encontrada.");
+		}
+	}
+}
+
+
+
+
